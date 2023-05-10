f16d114902fbb368bb3bed52de1ba1759074bdf5
Dmitry Batishchev <dmbatishchev@metapix.ai>
2023-05-10 18:55:47 +0300

    rename for test_1 and test_2

--- starlette_example/tests_main_service/test_for_server.py
+++ starlette_example/tests_main_service/test_for_server.py
@@ -1,8 +1,15 @@
 from starlette import status
 
 
-def test_greetings_new(test_client):
+def test_1(test_client):
 
     response = test_client.get('/app/hello_people')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.text == 'Hello Dima, Hello Denis'
+
+
+def test_2(test_client):
+
+    response = test_client.request('GET', '/app/hello_people')
     assert response.status_code == status.HTTP_200_OK
     assert response.text == 'Hello Dima, Hello Denis'
