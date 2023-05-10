b0fbe19f3b616e7bd6ed62d7a6ac286e18e71d90
Dmitry Batishchev <dmbatishchev@metapix.ai>
2023-05-10 19:00:53 +0300

    rename for test_1 and test_2

--- starlette_example/tests_main_service/test_for_server.py
+++ starlette_example/tests_main_service/test_for_server.py
@@ -13,3 +13,4 @@ def test_2(test_client):
     response = test_client.request('GET', '/app/hello_people')
     assert response.status_code == status.HTTP_200_OK
     assert response.text == 'Hello Dima, Hello Denis'
+
