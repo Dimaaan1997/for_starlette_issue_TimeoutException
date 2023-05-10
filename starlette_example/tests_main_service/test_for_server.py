012dcec1c0a0b6186dd49694dfd534f7ace2f724
Dmitry Batishchev <dmbatishchev@metapix.ai>
2023-05-10 19:14:40 +0300

    adding comment

--- starlette_example/tests_main_service/test_for_server.py
+++ starlette_example/tests_main_service/test_for_server.py
@@ -2,15 +2,13 @@ from starlette import status
 
 
 def test_1(test_client):
-
     response = test_client.get('/app/hello_people')
     assert response.status_code == status.HTTP_200_OK
     assert response.text == 'Hello Dima, Hello Denis'
 
 
+# this is new style of test with using "request" method with test client, as you recommended
 def test_2(test_client):
-
     response = test_client.request('GET', '/app/hello_people')
     assert response.status_code == status.HTTP_200_OK
     assert response.text == 'Hello Dima, Hello Denis'
-
